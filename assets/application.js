ea78c401a61c66666ed470f444b99f81575171dd
premnath pichaimuthu <premwebdev+dev@gmail.com>
2023-09-05 11:58:37 +0530

    j

--- assets/application.js
+++ assets/application.js
@@ -64,26 +64,3 @@
 
 })();
 
-// 
-document.addEventListener("DOMContentLoaded", function () {
-  
-
-  const column1 = document.getElementById("column1");
-  const column2Content = document.getElementById("column2-content");
-
-  function getTitleFromAttribute(attributeValue) {
-    const start = attributeValue.indexOf("title:") + 7;
-    const end = attributeValue.indexOf("}", start);
-    return attributeValue.substring(start, end);
-  }
-
-  column1.addEventListener("click", (event) => {
-    const clickedElement = event.target.closest(".contextblogrow11");
-    if (clickedElement) {
-      const titleData = clickedElement.getAttribute("data-context");
-      const title = getTitleFromAttribute(titleData);
-      console.log(title);
-      column2Content.textContent = `Information related to "${title}" goes here.`;
-    }
-  });
-});
